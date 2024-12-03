fbd03b3d66fa918bf2af439c65ff46b977f5b843
maahin2005 <mahin.malek2005@gmail.com>
2024-12-03 11:46:34 +0530

    Landing Page():Initiate Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import Navbar from "./../components/Navbar";
 export default function Home() {
-  return <div>Hello This is homepage</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="h-16  flex justify-center items-center">
+      <div className="flex justify-between items-center w-4/5 h-full">
+        <h1 className="text-4xl font-semibold bg-gradient-to-r from-[#FD6003] to-[#FB8E0B] bg-clip-text text-transparent font-sans">
+          Letschat
+        </h1>
+        <div></div>
+        <div className="flex items-center gap-5">
+          <h1>Login</h1>
+          <button className="min-w-36 w-full p-2 h-full bg-[#FB8E0B] text-white rounded-md">
+            Get Started free
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
