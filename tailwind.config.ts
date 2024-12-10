8467857c7f8517974f0edbdf065fab5b531ad34b
maahin2005 <mahin.malek2005@gmail.com>
2024-12-10 12:22:30 +0530

    Integration():Google-Font and Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,6 +60,11 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        ubuntu: "Ubuntu",
+        spaceGro: "Space Grotesk",
+        kanit: "Kanit",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
