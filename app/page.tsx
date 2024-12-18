e600ab94f88eb72f15d1b09b5342a0bb716db046
amit murmu <your.akmurmu6580@google.com>
2024-12-18 11:47:37 +0530

    removed client side oprations to make it SSR-ed component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from "./../components/Navbar";
+import Navbar from "@/components/Navbar";
 import Footer from "./../components/Footer";
 import Header from "./../components/home/Header";
 import FeaturesSection from "./../components/home/FeaturesSection";
-
 import Testimonial from "./../components/home/Testimonial";
-
 import Conversation from "@/components/home/Conversation";
 import Upcoming from "@/components/home/Upcoming";
 import CustomerHub from "@/components/home/CustomerHub";
