5bd5ba5f3597246b7055a9351334582db2cd2bf7
amit murmu <your.akmurmu6580@google.com>
2024-12-26 10:55:28 +0530

    working on OTP ui

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineWechatWork } from "react-icons/ai";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import { useSession, signIn } from "next-auth/react";
